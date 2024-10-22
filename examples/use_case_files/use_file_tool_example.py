a400f34bc427ed50941d1e5e228cb7f913e289ea
Oleksii Babych <olbychos@gmail.com>
2024-10-22 17:19:53 +0200

    feat: add workflow usage

--- examples/use_case_files/use_file_tool_example.py
+++ examples/use_case_files/use_file_tool_example.py
@@ -1,11 +1,18 @@
+import json
 from pathlib import Path
 
+from dynamiq import Workflow
+from dynamiq.callbacks import TracingCallbackHandler
 from dynamiq.connections import E2B
+from dynamiq.flows import Flow
 from dynamiq.nodes.agents import FileDataModel
 from dynamiq.nodes.agents.react import ReActAgent
 from dynamiq.nodes.tools.e2b_sandbox import E2BInterpreterTool
+from dynamiq.runnables import RunnableConfig
+from dynamiq.utils import JsonWorkflowEncoder
 from examples.llm_setup import setup_llm
-from examples.tools.file_reader import FileReaderTool
+
+INPUT_PROMPT = "Calculate the mean values for all columns in the CSV"
 
 
 def read_file_as_bytes(file_path: str) -> bytes:
@@ -32,6 +39,37 @@ def read_file_as_bytes(file_path: str) -> bytes:
 
     return file_content
 
+def run_workflow(
+    agent: ReActAgent,
+    input_prompt: str
+) -> tuple[str, dict]:
+    """
+    Execute a workflow using the ReAct agent to process a predefined query.
+
+    Returns:
+        tuple[str, dict]: The generated content by the agent and the trace logs.
+
+    Raises:
+        Exception: Captures and prints any errors during workflow execution.
+    """
+    tracing = TracingCallbackHandler()
+    wf = Workflow(flow=Flow(nodes=[agent]))
+
+    try:
+        result = wf.run(
+            input_data={"input": input_prompt},
+            config=RunnableConfig(callbacks=[tracing]),
+        )
+        # Verify that traces can be serialized to JSON
+        json.dumps(
+            {"runs": [run.to_dict() for run in tracing.runs.values()]},
+            cls=JsonWorkflowEncoder,
+        )
+
+        return result.output[agent.id]["output"]["content"], tracing.runs
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "", {}
 
 # Define file paths
 CSV_PATH = ".data/sample_regression_data.csv"
@@ -43,7 +81,6 @@ csv_bytes = read_file_as_bytes(CSV_PATH)
 file_csv_model = FileDataModel(file=csv_bytes, description="CSV file with regression data")
 
 # Initialize tools
-tool_csv = FileReaderTool(files=[file_csv_model])
 python_tool = E2BInterpreterTool(connection=E2B())
 
 # Set up LLM
@@ -58,9 +95,14 @@ agent = ReActAgent(
 )
 
 # Agent execution with input data and files
-result = agent.run(
-    input_data={"input": "Calculate the mean values for all columns in the CSV", "files": [file_csv_model]}
-)
+result = agent.run(input_data={"input": INPUT_PROMPT, "files": [file_csv_model]})
 
 # Print the result content
 print(result.output.get("content"))
+
+output, traces = run_workflow(
+    agent=agent,
+    input_prompt=INPUT_PROMPT
+)
+print("Agent Output:", output)
+
