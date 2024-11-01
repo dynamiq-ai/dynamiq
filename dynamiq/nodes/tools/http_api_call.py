21ba94bfd2e790b1366c476cb140fbc6bf5f9cea
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2024-11-01 18:45:54 +0200

    fix: change error type

--- dynamiq/nodes/tools/http_api_call.py
+++ dynamiq/nodes/tools/http_api_call.py
@@ -53,7 +53,7 @@ class HttpApiCall(ConnectionNode):
         response_type(ResponseType|str): The type of response content.
     """
 
-    name: str = "api-call-tool"
+    name: str = "Api Call Tool"
     description: str = "The description of the API call tool"
     group: Literal[NodeGroup.TOOLS] = NodeGroup.TOOLS
     connection: HttpConnection
@@ -101,7 +101,7 @@ class HttpApiCall(ConnectionNode):
         )
 
         if response.status_code not in self.success_codes:
-            raise ToolExecutionException(
+            raise ValueError(
                 f"Request failed with unexpected status code: {response.status_code} and response: {response.text}"
             )
 
