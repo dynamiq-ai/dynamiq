ff1882b45a11328059146fb9f428df239746d1cd
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2024-12-18 21:34:16 +0200

    fix: change location

--- dynamiq/nodes/agents/orchestrators/__init__.py
+++ dynamiq/nodes/agents/orchestrators/__init__.py
@@ -2,5 +2,6 @@ from .adaptive import AdaptiveOrchestrator
 from .adaptive_manager import AdaptiveAgentManager
 from .graph import GraphOrchestrator
 from .graph_manager import GraphAgentManager
+from .graph_state import GraphState
 from .linear import LinearOrchestrator
 from .linear_manager import LinearAgentManager
