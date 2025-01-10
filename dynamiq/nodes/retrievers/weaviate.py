6e5e9dcf81cc08bce06e015e830bd3175e219535
Taras Yaroshko <91616317+tyaroshko@users.noreply.github.com>
2025-01-10 16:05:52 +0200

    feat: add hybrid search for pgvector and Weaviate (#104)

--- dynamiq/nodes/retrievers/weaviate.py
+++ dynamiq/nodes/retrievers/weaviate.py
@@ -7,9 +7,10 @@ from dynamiq.nodes.node import ensure_config
 from dynamiq.nodes.retrievers.base import Retriever, RetrieverInputSchema
 from dynamiq.runnables import RunnableConfig
 from dynamiq.storages.vector import WeaviateVectorStore
+from dynamiq.storages.vector.weaviate.weaviate import WeaviteRetrieverVectorStoreParams
 
 
-class WeaviateDocumentRetriever(Retriever):
+class WeaviateDocumentRetriever(Retriever, WeaviteRetrieverVectorStoreParams):
     """Document Retriever using Weaviate.
 
     This class implements a document retriever that uses Weaviate as the vector store backend.
@@ -91,7 +92,17 @@ class WeaviateDocumentRetriever(Retriever):
         filters = input_data.filters or self.filters
         top_k = input_data.top_k or self.top_k
 
-        output = self.document_retriever.run(query_embedding, filters=filters, top_k=top_k, content_key=content_key)
+        alpha = input_data.alpha or self.alpha
+        query = input_data.query
+
+        output = self.document_retriever.run(
+            query_embedding,
+            filters=filters,
+            top_k=top_k,
+            content_key=content_key,
+            query=query,
+            alpha=alpha,
+        )
 
         return {
             "documents": output["documents"],
