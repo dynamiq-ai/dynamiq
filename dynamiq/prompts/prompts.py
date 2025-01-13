e7e5da1b378b1a9633028498e81833a378355c0c
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2025-01-13 18:51:52 +0200

    fix: add type check

--- dynamiq/prompts/prompts.py
+++ dynamiq/prompts/prompts.py
@@ -245,7 +245,7 @@ class Prompt(BasePrompt):
                     else:
                         raise ValueError(f"Invalid content type: {content.type}")
 
-                if len(out_msg_content) == 1:
+                if len(out_msg_content) == 1 and out_msg_content[0]["type"] == VisionMessageType.TEXT:
                     out_msg_content = out_msg_content[0]["text"]
 
                 out_msg = {
