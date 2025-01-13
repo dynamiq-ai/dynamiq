fc61b3672ce01d07ab7f2d94aa4f6071dd9459d0
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2025-01-13 18:41:19 +0200

    fix: change message convertion

--- dynamiq/prompts/prompts.py
+++ dynamiq/prompts/prompts.py
@@ -244,6 +244,10 @@ class Prompt(BasePrompt):
                         )
                     else:
                         raise ValueError(f"Invalid content type: {content.type}")
+
+                if len(out_msg_content) == 1:
+                    out_msg_content = out_msg_content[0]["text"]
+
                 out_msg = {
                     "content": out_msg_content,
                     "role": msg.role,
