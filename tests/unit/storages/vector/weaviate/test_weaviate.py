fd4d4c76db85bc453cbc9bbacb2a5e847d8350ec
MaksymAI <maksym@getdynamiq.ai>
2025-02-20 20:46:50 +0100

    test: update tests

--- tests/unit/storages/vector/weaviate/test_weaviate.py
+++ tests/unit/storages/vector/weaviate/test_weaviate.py
@@ -1,5 +1,4 @@
 import pytest
-
 from dynamiq.storages.vector.weaviate.weaviate import WeaviateVectorStore
 
 
@@ -24,9 +23,22 @@ def test_valid_collection_names(name: str) -> None:
         "123ABC",  # starts with a digit
         "_ABC",  # starts with underscore
         "A!B",  # invalid character
-        "aValid",  # lowercase first letter
         "A-Collection",  # contains a hyphen which is not allowed
     ],
 )
 def test_invalid_collection_names(name: str) -> None:
     assert WeaviateVectorStore.is_valid_collection_name(name) is False
+
+
+def test_fix_and_validate_index_name_already_valid() -> None:
+    # Name already valid; will be returned unchanged.
+    input_name = "ValidIndex"
+    fixed = WeaviateVectorStore._fix_and_validate_index_name(input_name)
+    assert fixed == input_name
+
+
+def test_fix_and_validate_index_name_lowercase() -> None:
+    # Name starts with lowercase; should automatically be fixed.
+    input_name = "default"
+    fixed = WeaviateVectorStore._fix_and_validate_index_name(input_name)
+    assert fixed == "Default"
