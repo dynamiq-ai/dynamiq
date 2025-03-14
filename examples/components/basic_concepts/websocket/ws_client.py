edac8eb2d972bf98c3890f742d30c5d91cd0990a
mousey <91616572+anastasiasenyk@users.noreply.github.com>
2025-03-14 03:18:24 +0200

    chore: restructure component examples

---
+++ examples/components/basic_concepts/websocket/ws_client.py
@@ -0,0 +1,74 @@
+import asyncio
+import logging
+from collections import defaultdict
+
+import websockets
+
+from dynamiq.nodes.tools.human_feedback import HFStreamingInputEventMessage, HFStreamingInputEventMessageData
+from dynamiq.types.streaming import StreamingEventMessage
+from examples.components.basic_concepts.websocket.ws_server_fastapi import HF_NODE_STREAMING_EVENT, HOST, PORT, WF_ID
+
+logger = logging.getLogger(__name__)
+
+
+WS_URI = f"ws://{HOST}:{PORT}/workflows/test"
+INPUT = {"a": 1, "b": 2, "input": "How old are you?"}
+WF_RUNS = 3
+
+
+async def websocket_client():
+    wf_runs = 0
+    async with websockets.connect(WS_URI) as websocket:
+        output_stream = defaultdict(str)
+        # Run WF
+        wf_run_event = StreamingEventMessage(entity_id=WF_ID, data=INPUT)
+        await websocket.send(wf_run_event.to_json())
+        wf_runs += 1
+
+        # Keep receiving messages from the server
+        try:
+            while True:
+                event_raw = await websocket.recv()
+                # await websocket.close()
+                try:
+                    event = StreamingEventMessage.model_validate_json(event_raw)
+                except Exception as e:
+                    logger.error(f"Error while parsing message. Error: {e}")
+
+                message = event.data
+                if event.entity_id == WF_ID:
+                    logger.info(f"Final output {event.entity_id}: {message}")
+                    if wf_runs >= WF_RUNS:
+                        break
+
+                    # Run WF again
+                    wf_run_event = StreamingEventMessage(entity_id=WF_ID, data=INPUT)
+                    await websocket.send(wf_run_event.to_json())
+                    wf_runs += 1
+
+                elif event.event == HF_NODE_STREAMING_EVENT:
+                    # Send a user input to the server back
+                    hf_user_content = input(event.data["prompt"])
+                    hf_response_event = HFStreamingInputEventMessage(
+                        wf_run_id=event.wf_run_id,
+                        entity_id=event.entity_id,
+                        data=HFStreamingInputEventMessageData(content=hf_user_content),
+                        event=event.event,
+                    )
+                    await websocket.send(hf_response_event.to_json())
+                    logger.info(f"Streaming {event.wf_run_id} - {event.entity_id}: {hf_user_content}")
+                else:
+                    if content := message["choices"][0]["delta"]["content"]:
+                        output_stream[event.entity_id] += content
+                        logger.info(
+                            f"Streaming {event.wf_run_id} - {event.entity_id}: {output_stream[event.entity_id]}"
+                        )
+
+        except websockets.ConnectionClosed:
+            logger.error("WebSocket connection closed by the server")
+
+        await websocket.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(websocket_client())
---
+++ examples/components/tools/custom_tools/calculator_sympy.py
@@ -0,0 +1,66 @@
+from typing import Any, Literal
+
+import sympy as sp
+from pydantic import ConfigDict
+
+from dynamiq.nodes import NodeGroup
+from dynamiq.nodes.node import Node, ensure_config
+from dynamiq.runnables import RunnableConfig
+from dynamiq.utils.logger import logger
+
+
+class CalculatorTool(Node):
+    """
+    A tool to evaluate mathematical expressions.
+
+    Attributes:
+        group (Literal[NodeGroup.TOOLS]): The group of the node.
+        name (str): The name of the tool.
+        description (str): The description of the tool.
+    """
+
+    group: Literal[NodeGroup.TOOLS] = NodeGroup.TOOLS
+    name: str = "Calculator"
+    description: str = (
+        "Tool to evaluate mathematical expressions. Provide a raw string of the math operation to parse and evaluate."
+    )
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def _evaluate_expression(self, expression: str) -> Any:
+        """
+        Parse and evaluate the given mathematical expression.
+
+        Args:
+            expression (str): The mathematical expression to evaluate.
+
+        Returns:
+            Any: The result of the evaluated expression or an error message if evaluation fails.
+        """
+        try:
+            result = sp.sympify(expression)
+            return result
+        except Exception as e:
+            return str(e)
+
+    def execute(self, input_data: dict[str, Any], config: RunnableConfig = None, **kwargs) -> dict[str, Any]:
+        """
+        Execute the tool with the provided input data and configuration.
+
+        Args:
+            input_data (dict[str, Any]): The input data containing the expression to evaluate.
+            config (RunnableConfig, optional): The configuration for the runnable. Defaults to None.
+
+        Returns:
+            dict[str, Any]: The result of the evaluation.
+        """
+        logger.debug(f"Tool {self.name} - {self.id}: started with input data {input_data}")
+
+        config = ensure_config(config)
+        self.run_on_node_execute_run(config.callbacks, **kwargs)
+
+        expression = input_data.get("input", "")
+        result = self._evaluate_expression(expression)
+
+        logger.debug(f"Tool {self.name} - {self.id}: finished with result {result}")
+        return {"content": result}
