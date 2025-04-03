5bb7eded3e6a4ef70e2a2d8786c767b7ca6f2ae5
Andrii Baida <andrii.baida@getdynamiq.ai>
2025-04-03 19:27:15 +0300

    feat: add async functionality for runnables

--- dynamiq/utils/__init__.py
+++ dynamiq/utils/__init__.py
@@ -1,2 +1,2 @@
 from .duration import format_duration
-from .utils import JsonWorkflowEncoder, format_value, generate_uuid, merge, serialize
+from .utils import JsonWorkflowEncoder, format_value, generate_uuid, is_called_from_async_context, merge, serialize
