755cebccc62b8f1aee2052b49a310e06f0cea04f
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2025-04-18 11:53:45 +0300

    feat: add role template tests

--- tests/integration/nodes/agents/test_agent_input_message.py
+++ tests/integration/nodes/agents/test_agent_input_message.py
@@ -19,9 +19,11 @@ from dynamiq.prompts import (
 
 REQUEST_TEMPLATE = "Request: {{request}}"
 URL_TEMPLATE = "{{url}}"
+CONTEXT_TEMPLATE = "{{context}}"
 
 REQUEST = "request_placeholder"
 URL = "url_placeholder"
+CONTEXT = "context_placeholder"
 
 
 @pytest.fixture
@@ -37,19 +39,25 @@ def model():
     )
 
 
-def create_react_agent(llm, inference_mode, input_message=None):
+def create_react_agent(llm, inference_mode, input_message=None, role=None):
     """ReActAgent"""
-    return ReActAgent(name="Test Agent", llm=llm, tools=[], input_message=input_message, inference_mode=inference_mode)
+    return ReActAgent(
+        name="Test Agent", llm=llm, tools=[], input_message=input_message, role=role, inference_mode=inference_mode
+    )
 
 
-def create_simple_agent(llm, inference_mode, input_message=None):
+def create_simple_agent(llm, inference_mode, input_message=None, role=None):
     """SimpleAgent"""
-    return SimpleAgent(name="Test Agent", llm=llm, inference_mode=inference_mode, input_message=input_message)
+    return SimpleAgent(
+        name="Test Agent", llm=llm, inference_mode=inference_mode, role=role, input_message=input_message
+    )
 
 
-def create_reflection_agent(llm, inference_mode, input_message=None):
+def create_reflection_agent(llm, inference_mode, input_message=None, role=None):
     """ReflectionAgent"""
-    return ReflectionAgent(name="Test Agent", llm=llm, inference_mode=inference_mode, input_message=input_message)
+    return ReflectionAgent(
+        name="Test Agent", llm=llm, inference_mode=inference_mode, role=role, input_message=input_message
+    )
 
 
 @pytest.mark.parametrize(
@@ -94,25 +102,29 @@ def test_simple_agent_workflow(model, inference_mode):
 )
 def test_custom_agent_workflow(model, inference_mode):
     input_message = Message(content=REQUEST_TEMPLATE, role=MessageRole.USER)
-    agent = create_react_agent(model, inference_mode, input_message)
 
-    agent.run(
-        input_data={"request": REQUEST},
+    react_agent = create_react_agent(model, inference_mode, input_message, role=CONTEXT_TEMPLATE)
+
+    react_agent.run(
+        input_data={"request": REQUEST, "context": CONTEXT},
     )
     expected_result = input_message.format_message(request=REQUEST)
-    assert agent._prompt.messages[1] == expected_result
+    assert react_agent._prompt.messages[1] == expected_result
+    assert CONTEXT in react_agent._prompt.messages[0].content
 
-    simple_agent = create_simple_agent(model, inference_mode, input_message)
+    simple_agent = create_simple_agent(model, inference_mode, input_message, role=CONTEXT_TEMPLATE)
     simple_agent.run(
-        input_data={"request": REQUEST},
+        input_data={"request": REQUEST, "context": CONTEXT},
     )
     assert simple_agent._prompt.messages[1] == expected_result
+    assert CONTEXT in simple_agent._prompt.messages[0].content
 
-    reflection_agent = create_reflection_agent(model, inference_mode, input_message)
+    reflection_agent = create_reflection_agent(model, inference_mode, input_message, role=CONTEXT_TEMPLATE)
     reflection_agent.run(
-        input_data={"request": REQUEST},
+        input_data={"request": REQUEST, "context": CONTEXT},
     )
     assert reflection_agent._prompt.messages[1] == expected_result
+    assert CONTEXT in reflection_agent._prompt.messages[0].content
 
 
 @pytest.mark.parametrize(
