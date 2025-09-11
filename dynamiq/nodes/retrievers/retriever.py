0bea90542ab12be5991d2f2bc0c6c002b90cc127
Tetiana <tetiana.trachuk.05@gmail.com>
2025-09-11 14:12:53 +0300

    feat: update top_k param passing to retrievers

--- dynamiq/nodes/retrievers/retriever.py
+++ dynamiq/nodes/retrievers/retriever.py
@@ -19,7 +19,7 @@ class VectorStoreRetrieverInputSchema(BaseModel):
     filters: dict[str, Any] = Field(
         default_factory=dict, description="Parameter to provide filters to apply for retrieving specific documents."
     )
-    top_k: int = Field(default=None, description="Parameter to provided how many documents to retrieve.")
+    top_k: int = Field(default=0, description="Parameter to provided how many documents to retrieve.")
 
 
 class VectorStoreRetriever(Node):
@@ -166,7 +166,7 @@ class VectorStoreRetriever(Node):
             document_retriever_output = self.document_retriever.run(
                 input_data={
                     "embedding": embedding,
-                    "top_k": top_k,
+                    **({"top_k": top_k} if top_k else {}),
                     "filters": filters,
                     "alpha": alpha,
                     **({"query": query} if alpha else {}),
