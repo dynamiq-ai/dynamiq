1c05ad1d3ad9f7518afeaa5671ac3b0f68ea5bcb
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2025-10-10 16:01:36 +0300

    fix: description

--- dynamiq/nodes/utils/utils.py
+++ dynamiq/nodes/utils/utils.py
@@ -29,7 +29,7 @@ class Input(Pass):
             "query": {
                 "type": "Any"
             },
-            "files: {
+            "files": {
                 "type": "list[files]"
             }
         }
