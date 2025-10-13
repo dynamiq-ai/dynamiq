09873935e57ac6d402af78660123d45a4b91be0a
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2025-10-13 12:46:19 +0300

    fix: add function

--- dynamiq/utils/workflow_generation/__init__.py
+++ dynamiq/utils/workflow_generation/__init__.py
@@ -1,2 +1,2 @@
 from .mock_knowledgebase import KnowledgebaseRetriever
-from .node_generation import generate_data_from_schema, generate_node
+from .node_generation import add_connection, generate_data_from_schema, generate_node
