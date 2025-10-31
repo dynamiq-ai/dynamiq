6ccc4de401324027bc686bf09fd7261f37ead6b9
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2025-10-31 15:39:07 +0200

    feat: add vector store write pipeline example

--- examples/components/agents/agents/agent_vector_store_write_pipeline.py
+++ examples/components/agents/agents/agent_vector_store_write_pipeline.py
@@ -2,9 +2,13 @@ import json
 
 from dynamiq import Workflow
 from dynamiq.callbacks import TracingCallbackHandler
+from dynamiq.components.splitters.document import DocumentSplitBy
+from dynamiq.connections import Firecrawl as FirecrawlConnection
+from dynamiq.connections import Tavily as TavilyConnection
 from dynamiq.flows import Flow
 from dynamiq.nodes.agents import Agent
 from dynamiq.nodes.embedders import OpenAIDocumentEmbedder
+from dynamiq.nodes.tools import FirecrawlTool, PreprocessTool, TavilyTool
 from dynamiq.nodes.types import InferenceMode
 from dynamiq.nodes.writers import WeaviateDocumentWriter
 from dynamiq.nodes.writers.writer import VectorStoreWriter
@@ -13,34 +17,58 @@ from dynamiq.storages.vector import WeaviateVectorStore
 from dynamiq.utils import JsonWorkflowEncoder
 from examples.llm_setup import setup_llm
 
+AGENT_ROLE = """
+You are an AI assistant that assists the users with their questions.
+If the user asks you to ingest a document, \
+    you should process your response with the PreprocessTool \
+    and write the results to the vector store in a structured format.
+"""
+
 
 def setup_agent() -> Agent:
     document_embedder = OpenAIDocumentEmbedder(model="text-embedding-3-small")
-    vector_store_writer = WeaviateDocumentWriter(
-        vector_store=WeaviateVectorStore(index_name="TravelData", create_if_not_exist=True)
+    writer = WeaviateDocumentWriter(
+        vector_store=WeaviateVectorStore(index_name="ArticlesData", create_if_not_exist=True)
     )
 
-    tool_vector_store_writer = VectorStoreWriter(
-        name="Travel Data Vector Store Writer",
+    tool_writer = VectorStoreWriter(
+        name="Articles Data Vector Store Writer",
         document_embedder=document_embedder,
-        document_writer=vector_store_writer,
+        document_writer=writer,
         is_optimized_for_agents=True,
     )
 
-    llm = setup_llm(model_name="gpt-5-mini")
+    preprocess_tool = PreprocessTool(
+        split_by=DocumentSplitBy.SENTENCE,
+        split_length=10,
+        split_overlap=0,
+    )
+
+    tavily_tool = TavilyTool(
+        connection=TavilyConnection(),
+        include_raw_content=True,
+    )
+
+    scraping_tool = FirecrawlTool(
+        connection=FirecrawlConnection(),
+        include_raw_content=True,
+    )
+
+    llm = setup_llm(model_provider="gemini", model_name="gemini-2.5-flash-preview-05-20")
 
     agent = Agent(
         name="React Agent",
         llm=llm,
-        tools=[tool_vector_store_writer],
+        tools=[tool_writer, preprocess_tool, tavily_tool, scraping_tool],
         inference_mode=InferenceMode.XML,
-        role="AI assistant with deep knowledge about various travel destinations. Your goal is to provide well explained answers and write them to the vector store using the Travel Data Vector Store Writer tool",  # noqa: E501
-        max_loops=7,
+        parallel_tool_calls_enabled=True,
+        role=AGENT_ROLE,
+        max_loops=50,
     )
     return agent
 
 
-AGENT_QUERY = """What are top 10 places to visit in Rome, Italy?"""
+AGENT_QUERY = """Ingest Paul Graham's "Life is Short" essay into the vector store."""
 
 
 def run_workflow(agent: Agent = setup_agent(), input_prompt: str = AGENT_QUERY) -> tuple[str, dict]:
