b1af9588a5962e849573857df2780335fc884125
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2025-11-13 16:57:31 +0200

    feat: add OpenSearch vector storage

--- dynamiq/storages/vector/__init__.py
+++ dynamiq/storages/vector/__init__.py
@@ -1,6 +1,7 @@
 from .chroma import ChromaVectorStore
 from .elasticsearch import ElasticsearchVectorStore
 from .milvus import MilvusVectorStore
+from .opensearch import OpenSearchVectorStore
 from .pgvector import PGVectorStore
 from .pinecone import PineconeVectorStore
 from .qdrant import QdrantVectorStore
