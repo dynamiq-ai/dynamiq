a8dfa2af6808480958817c6eede49794da5874cf
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2026-04-07 05:42:17 +0300

    fix: update imports for sandbox classes

--- dynamiq/sandboxes/e2b.py
+++ dynamiq/sandboxes/e2b.py
@@ -10,7 +10,7 @@ from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_ex
 
 from dynamiq.connections import E2B
 from dynamiq.nodes import Node
-from dynamiq.nodes.tools.e2b_sandbox import SandboxCreationErrorHandling
+from dynamiq.nodes.tools.code_interpreter import SandboxCreationErrorHandling
 from dynamiq.sandboxes.base import Sandbox, SandboxInfo, ShellCommandResult
 from dynamiq.sandboxes.exceptions import SandboxConnectionError
 from dynamiq.utils.logger import logger
