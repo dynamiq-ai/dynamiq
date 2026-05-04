0fb9d5394a62e0ff576c02fe562fc161632b3346
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2026-05-04 12:05:17 +0300

    feat: add postgres backend

--- dynamiq/checkpoints/backends/__init__.py
+++ dynamiq/checkpoints/backends/__init__.py
@@ -1,3 +1,4 @@
 from dynamiq.checkpoints.backends.base import CheckpointBackend
 from dynamiq.checkpoints.backends.filesystem import FileSystem
 from dynamiq.checkpoints.backends.in_memory import InMemory
+from dynamiq.checkpoints.backends.postgresql import PostgreSQL
